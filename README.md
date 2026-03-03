0ed78187fdfabff1e2aee293b3bbb6c88b594d1e
Ddav3 <saladavidee@gmail.com>
2026-03-03 20:55:56 +0100

    implemented graphic visualization of data based on state over the years on Dataset 1: removed the states of which there are missing data or not of all the 27 years recorded

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # BEES PRODUCTION ANALYSIS 
-The code analyses a dataset containing info mainly related to the production of honey from bees' colonies situated in US countries from 1995 to 2021. 
+The code analyses a dataset containing info mainly related to the production of honey from bees' colonies situated in US from 1995 to 2021. 
 The dataset is examined showing the evolution in the production of honey and the number of colonies over the years and depending on the area. After that, a prediction on the possible number of colonies/quantity of honey produced for the next 10 years is realized through a linear regression model. 
 
 This main dataset is then combined with two others.
@@ -10,13 +10,22 @@ By merging these two, the code checks the correlation between the pesticides ado
 The second one shows the weather's effects on the beehives in some countries of US. The analysis is made based on hourly observations on apiaries during 2016-2017.
 The combination of the two datasets aims at understanding the influence of weather and temperatures on the change in beehive number, other than being able to classify a beehive as "healthy"/"unhealthy", based on the given data.
 
+Utility functions are kept in a separate file.
 The content is presented using streamlit. 
 
 ## Setup section 
 The initial block of code contains is a procedure to set up libraries and collect the datasets from kaggle website, using kagglehub. Should this method not work (for whatever reason, such as problems of the website), a copy updated to the version during which the code was written is used and kept in a local folder. 
 
-## Part 1: Honey production Dataset
-The second block is dedicated to the analysis of the first, main dataset, regarding the evolution in the quantity of honey produced by the colonies in US.  
+## Part 1: Honey production
+The first part is dedicated to the analysis of the first, main dataset, regarding the evolution in the quantity of honey produced by the colonies in US.  
+
+
+## Part 2: Toxicity of Pesticide on Bees (Apistox)
+This part of the code illustrates the second dataset regarding the  
+
+
+## Part 3: Weather Effects on Bees' Health
+Last part involves the dataset folder regarding the study of the weather effects on the bees colony health. 
 
 ## Datasets
 - honey production dataset: https://www.kaggle.com/datasets/mohitpoudel/us-honey-production-19952021
--- utils.py
+++ utils.py
@@ -1,17 +1,95 @@
 import pandas as pd
+import kagglehub as kgh
+from kagglehub import KaggleDatasetAdapter
+import pandas as pd
+import os
 
-def EDA(dataframe: pd.DataFrame):
+def load_bees_datasets()-> tuple[pd.DataFrame, pd.DataFrame, dict[pd.DataFrame]]:
+    try:
+
+        ## First Dataset ##
+
+        honey_production_df = kgh.dataset_load(
+            KaggleDatasetAdapter.PANDAS, 
+            "mohitpoudel/us-honey-production-19952021",
+            "US_honey_dataset_updated.csv"
+        )
+        print(f" honey_production_df {honey_production_df.shape} : Done! \n")
+
+
+        ## Second Dataset ##
+
+        apistox_df = kgh.dataset_load(
+            KaggleDatasetAdapter.PANDAS,
+            "baharabaz/apistox-dataset", 
+            "apistox/dataset_final.csv"
+        )
+        print(f" apistox_df {apistox_df.shape} : Done! \n")
+
+
+        ## Third Dataset (collected in a dictionary) later to be merged ##
+
+        #Accessing the subfolder in which the former datasets are stored
+        path = kgh.dataset_download("jocelyndumlao/predicting-honeybee-health-from-hive-and-weather")
+        subfolder = [elem for elem in os.listdir(path)]
+        datasets_folder = os.path.join(path,subfolder[0]) 
+
+        #Creating a dictionary of datasets in order to store them and make them easily accessible for comparison
+        bees_health_weather_dict = {}
+
+        for filename in os.listdir(datasets_folder):
+            bees_health_weather_dict[filename.replace(".csv", "")] = pd.read_csv(os.path.join(datasets_folder,filename))
+            print(f" {filename} {bees_health_weather_dict[filename.replace(".csv", "")].shape} : Done!")
+
+        print("\nDatasets collection completed.")
+
+    except Exception as e: 
+
+        #should the web access to kaggle not work, the local (possibly not updated) version can be used instead 
+        print(f"... {e}.\nProcedure stopped.")
+        print("-------------------------------\nLocal loading...")
+
+
+        ## First Dataset ##
+
+        honey_production_df = pd.read_csv("datasets/US_honey_production.csv")
+        print(f" honey_production_df {honey_production_df.shape} : Done! \n")
+
+
+        ## Second Dataset ##
+
+        apistox_df = pd.read_csv("datasets/apistox.csv")
+        print(f" apistox_df {apistox_df.shape} : Done! \n")
+
+
+        ## Third Dataset (collected in a dictionary) later to be merged ##
+
+        bees_health_weather_dict = {}
+        for filename in os.listdir("datasets/bees_health_on_weather"):
+            bees_health_weather_dict[filename.replace(".csv", "")] = pd.read_csv(os.path.join("datasets/bees_health_on_weather",filename))
+            print(f" {filename} {bees_health_weather_dict[filename.replace(".csv", "")].shape} : Done!")
+
+        print("\nSome problem occurred. Local datasets loaded instead.")
+    
+    return honey_production_df, apistox_df, bees_health_weather_dict
+
+
+def EDA(dataframe: pd.DataFrame, head: int = 5):
     '''
     A simple function used to easily implement the EDA in the input dataframe 
     '''
 
     print(
-        dataframe.info(), 
-        "\n##-------------------------------------------##\n",
+        "##-------------- INFO ------------------------------##\n")
+    dataframe.info()
+    print(
+        "\n\n##-------------- DESCRIBE --------------------------##\n",
         dataframe.describe(), 
-        "\n##-------------------------------------------##\n",
-        dataframe.head(),
-        "\n##-------------------------------------------##\n",
+        "\n\n##-------------- HEAD("+str(head) +") ---------------------------##\n",
+        dataframe.head(n = head),
+        "\n\n##-------------- CORRELATION -----------------------##\n",
+        dataframe.corr(numeric_only=True),
+        "\n\n##-------------- NUMBER OF NAN ---------------------##\n",
         "\nNumber of rows with null/NaN: " + str(dataframe.size - dataframe.dropna().size) + "\n\n"
     )
 
