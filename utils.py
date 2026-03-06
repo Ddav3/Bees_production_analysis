fe2181783d42c5d2e2a9cdc7211a92f390fa6cb1
Ddav3 <saladavidee@gmail.com>
2026-03-06 17:06:55 +0100

    adjusted the global plots of info and utils updated. Last commit before apistox analysis

--- utils.py
+++ utils.py
@@ -1,10 +1,22 @@
+import numpy as np
 import pandas as pd
 import kagglehub as kgh
 from kagglehub import KaggleDatasetAdapter
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 
 def load_bees_datasets()-> tuple[pd.DataFrame, pd.DataFrame, dict[pd.DataFrame]]:
+    '''
+    The function tries to connect to the Kaggle website, in order to load the Datasets used in the project (through Kagglehub).
+    Whenever this works correctly, the datasets are put in a variable and a message of successful result is showed. 
+    Should this procedure not work for whatever reason, a non-updated version of the Datasets, which is the first version used
+    when the code was first finished, is kept in the folder "datasets" and therefore used in their place. 
+    The 3 datasets contain:
+    - the honey production in US from 1995 to 2021
+    - the toxicity levels on bees
+    - the observations of weather effects on bees health
+    '''
     try:
 
         ## First Dataset ##
@@ -92,4 +104,33 @@ def EDA(dataframe: pd.DataFrame, head: int = 5):
         "\n\n##-------------- NUMBER OF NAN ---------------------##\n",
         "\nNumber of rows with null/NaN: " + str(dataframe.size - dataframe.dropna().size) + "\n\n"
     )
+
+
+def plot_all(dataframe: pd.DataFrame, by: str, x_col: str, y_col: str):
+    '''
+    Builds the subplots for a group of plots. The dimension is calculated considering the upper limit of the square root of 
+    the number of elements of which the second input's column is composed.
+    Inputs: 
+        dataframe: the Dataframe
+        by: the column from which the plots will be subdivided
+        x_col: in the Dataframe, the name of the column that will be used in the x axis
+        y_col: in the Dataframe, the name of the column containing the data for the y axis
+    '''
+    
+    elements = dataframe[by].unique()
+    rows = int(np.ceil(np.sqrt(len(elements)))) 
+    cols= rows
+    _, axes = plt.subplots(rows, cols, figsize =(20,20))
+
+    for index, element in enumerate(elements):
+        plot = axes[int(index/rows),index%cols]
+        filtered_part = dataframe[dataframe[by] == element]
+        
+        plot.plot(filtered_part[x_col], filtered_part[y_col])
+        plot.set_title(str(element))
+        plot.tick_params(axis ="both", labelsize = 9)
+        plot.grid(alpha = 0.3)
+
+    plt.tight_layout()
+    plt.show()
 
